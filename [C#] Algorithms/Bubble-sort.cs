cce416457464ffc96bc0b41f4d3dfd97b0ddcf7c
Grzegorz Sosna <grzegorz.sosna@o2.pl>
2022-06-18 17:15:27 +0200

    Add files via upload

---
+++ [C#] Algorithms/Bubble-sort.cs
@@ -0,0 +1,40 @@
+/* Napisać program sortujący wprowadzane z klawiatury liczby całkowite rosnąco. Program powinien zapytać 
+użytkownika o liczbę elementów do wprowadzenia <1,10>, zweryfikować wprowadzoną liczbę, wczytać liczby i 
+wyprowadzić posortowane elementy. Obsłużyć wyjątki. */
+
+using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static int[] BubbleSort(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[i] > array[j])
+                    {
+                        int buffor = array[j];
+                        array[j] = array[i];
+                        array[i] = buffor;
+                    }
+                }
+            }
+            return array;
+        }
+
+        static void Main(string[] args)
+        {           
+            int[] array = new int[10] { 3, 115, 74, 21, 45, 123, 2, 34, 85, 23 };
+            int[] sortedArray = BubbleSort(array);
+
+            Console.WriteLine("Tablica posortowana :");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(sortedArray[i]);
+            }
+        }
+    }
+}
