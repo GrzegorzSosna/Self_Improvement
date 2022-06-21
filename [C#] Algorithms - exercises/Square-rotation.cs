7369ba227e926ad7761a8aec4dcca3390a5545b4
Grzegorz Sosna <grzegorz.sosna@o2.pl>
2022-06-21 16:49:43 +0200

    Add files via upload

---
+++ [C#] Algorithms - exercises/Square-rotation.cs
@@ -0,0 +1,67 @@
+/* Dany jest rysunek reprezentowany przez macierz o wymiarach NxN, w którym kazdy piksel jest 
+reprezentowany za pomocą 4 bajtów. Napisz metodę, która rotuje rysunek o 90 stopni. Czy potrafisz
+wykonać tę operację w miejscu? */
+
+using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static int[,] RotateSquare(int[,] square, int squareSize)
+        {
+            int bufor;
+            int layer = squareSize / 2;
+            if (layer % 2 != 0)
+                layer++;
+            int last = squareSize - 1;
+
+            for (int first = 0; first < layer; first++)
+            {
+                for (int q = first; q < last - first; q++)
+                {
+                    bufor = square[first, last - q];
+                    // up = left
+                    square[first, last - q] = square[q, first];
+                    // left = down
+                    square[q, first] = square[last - first, q];
+                    // down = right
+                    square[last - first, q] = square[last - q, last - first];
+                    // right = up
+                    square[last - q, last - first] = bufor;
+                }
+            }
+            return square;
+        }
+
+        static void Main(string[] args)
+        {         
+            int squereSize = 6; // figure size e.g .: 2 is 2x2 or 6 is 6x6
+            int[,] square = new int[squereSize, squereSize];
+
+            for (int i = 0; i < squereSize; i++)
+            {
+                for (int q = 0; q <squereSize; q++)
+                {
+                    square[i, q] = i;
+                }          
+            }
+
+            while (true)
+            {
+                Console.Clear();
+                square = RotateSquare(square, squereSize);
+                for (int i = 0; i < squereSize; i++)
+                {
+                    for (int q = 0; q < squereSize; q++)
+                    {
+                        Console.Write(square[i, q]);
+                    }
+                    Console.WriteLine();
+                }
+                Console.WriteLine("\nPress the key to rotate the square 90 degrees.");
+                Console.ReadKey();
+            }
+        }
+    }
+}
