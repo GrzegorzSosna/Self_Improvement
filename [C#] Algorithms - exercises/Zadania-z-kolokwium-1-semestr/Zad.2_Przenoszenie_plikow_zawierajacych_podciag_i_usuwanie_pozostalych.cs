177a398f4e847a1228ea0db20ffe9d0392637965
Grzegorz Sosna <grzegorz.sosna@o2.pl>
2022-07-09 10:06:18 +0200

    Add files via upload

---
+++ [C#] Algorithms - exercises/Zadania-z-kolokwium-1-semestr/Zad.2_Przenoszenie_plikow_zawierajacych_podciag_i_usuwanie_pozostalych.cs
@@ -0,0 +1,33 @@
+using System;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void PrzenoszenieUsuwaniePlikow(string folderZrodlowy, string folderDocelowy)
+        {
+            string[] pliki = Directory.GetFiles(folderZrodlowy);
+
+            foreach (string plik in pliki)
+            {
+                FileInfo nowyPlik = new FileInfo(plik);
+                if (nowyPlik.Name.Contains("Maskowane")) 
+                {
+                    File.Move(folderZrodlowy + @"\" + nowyPlik.Name, folderDocelowy + @"\" + nowyPlik.Name);
+                }
+                else
+                    File.Delete(folderZrodlowy + @"\" + nowyPlik.Name);
+            }
+        }
+            
+
+        static void Main(string[] args)
+        {
+            string folderZrodlowy = @"C:\TestA";
+            string folderDocelowy = @"C:\TestB";
+
+            PrzenoszenieUsuwaniePlikow(folderZrodlowy, folderDocelowy);
+        }
+    }
+}
