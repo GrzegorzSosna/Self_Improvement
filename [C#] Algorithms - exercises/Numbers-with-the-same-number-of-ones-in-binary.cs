8b8fe916be6c3d00307dca92e77e5dd6ccbc36a6
Grzegorz Sosna <grzegorz.sosna@o2.pl>
2022-07-24 11:45:47 +0200

    Update Numbers-with-the-same-number-of-ones-in-binary.cs

--- [C#] Algorithms - exercises/Numbers-with-the-same-number-of-ones-in-binary.cs
+++ [C#] Algorithms - exercises/Numbers-with-the-same-number-of-ones-in-binary.cs
@@ -1,97 +1,100 @@
-/* Zad. Napisz kod, który przyjmuje dodatnią liczbę całkowitą i wyświetla najbliższą mniejszą i większą wartość
-o tej samej liczbie jedynek w reprezentacji binarnej. */
+/* Napisz kod, który przyjmuje dodatnią liczbę całkowitą i wyświetla najbliższą mniejszą i większą 
+wartość o tej samej liczbie jedynek w reprezentacji binarnej. */
 using System;
 
 namespace ConsoleApp
 {
-	class Program
-	{
-		static string BinaryNumber(double number, int numberOfBits)
-		{
-			string binaryNumber = null;
-			for (int i = numberOfBits - 1; i >= 0; i--)
-			{
-				if (number / Math.Pow(2, i) >= 1)
-				{
-					binaryNumber += "1";
-					number -= Math.Pow(2, i);
-				}
-				else
-					binaryNumber += "0";
-			}
-			return binaryNumber;
-		}
-
-		static int NumberOfOnes(double number, int numberOfBits)
+    class Program
+    {
+        /// <summary>function that converts a decimal number to a binary number</summary>
+        static string BinaryNumber(double number, int numberOfBits)
         {
-			int numberOfOnes = 0;
-			for (int i = numberOfBits - 1; i >= 0; i--)
-			{
-				if (number / Math.Pow(2, i) >= 1)
-				{
-					numberOfOnes++;
-					number -= Math.Pow(2, i);
-				}
-			}		
-			return numberOfOnes;
-		}
+            string binaryNumber = null;
+            for (int i = numberOfBits - 1; i >= 0; i--)
+            {
+                if (number / Math.Pow(2, i) >= 1)
+                {
+                    binaryNumber += "1";
+                    number -= Math.Pow(2, i);
+                }
+                else
+                    binaryNumber += "0";
+            }
+            return binaryNumber;
+        }
 
-		static void TheSameNumberOfOnes(double number, int numberOfBits)
+        /// <summary>a function that counts ones in a binary number</summary>
+        static int NumberOfOnes(double number, int numberOfBits)
         {
-			int numberOfOnes = NumberOfOnes(number, numberOfBits);
-			bool isItBigger = true, isItSmaller = true;
-			int numberOfOnesBigger = 0, numberOfOnesSmaller = 0;
-			string binaryNumberBigger = null, binaryNumberSmaller = null;
-
-			double numberTemp = number; 
-			while (numberOfOnes != numberOfOnesBigger)
+            int numberOfOnes = 0;
+            for (int i = numberOfBits - 1; i >= 0; i--)
             {
-				numberTemp++;
-				if (numberTemp > Math.Pow(2, numberOfBits - 1))
+                if (number / Math.Pow(2, i) >= 1)
                 {
-					isItBigger = false;
-					break;
-				}				
-				binaryNumberBigger = BinaryNumber(numberTemp, numberOfBits);
-				numberOfOnesBigger = NumberOfOnes(numberTemp, numberOfBits);
-			}
+                    numberOfOnes++;
+                    number -= Math.Pow(2, i);
+                }
+            }
+            return numberOfOnes;
+        }
 
-			numberTemp = number;
-			while (numberOfOnes != numberOfOnesSmaller)
-			{
-				numberTemp--;
-				if (numberTemp < 0)
+        /// <summary>a function that compares the number of ones</summary>
+        static void TheSameNumberOfOnes(double number, int numberOfBits)
+        {
+            int numberOfOnes = NumberOfOnes(number, numberOfBits);
+            bool isItBigger = true, isItSmaller = true;
+            int numberOfOnesBigger = 0, numberOfOnesSmaller = 0;
+            string binaryNumberBigger = null, binaryNumberSmaller = null;
+
+            double numberTemp = number;
+            while (numberOfOnes != numberOfOnesBigger)
+            {
+                numberTemp++;
+                if (numberTemp > Math.Pow(2, numberOfBits - 1))
                 {
-					isItSmaller = false;
-					break;
-				}					
-				binaryNumberSmaller = BinaryNumber(numberTemp, numberOfBits);
-				numberOfOnesSmaller = NumberOfOnes(numberTemp, numberOfBits);
-			}
-			Console.WriteLine("Liczba podana: " + BinaryNumber(number, numberOfBits));
-			if (isItBigger == true)
-				Console.WriteLine("Większa liczba: " + binaryNumberBigger);
-			else
-				Console.WriteLine("Nie znaleziono większej liczby spełniającej warunek w podanym zakresie.");
-			if (isItSmaller == true)
-				Console.WriteLine("Mniejsza liczba: " + binaryNumberSmaller);
-			else
-				Console.WriteLine("Nie znaleziono mniejszej liczby spełniającej warunek w podanym zakresie.");
-		}
+                    isItBigger = false;
+                    break;
+                }
+                binaryNumberBigger = BinaryNumber(numberTemp, numberOfBits);
+                numberOfOnesBigger = NumberOfOnes(numberTemp, numberOfBits);
+            }
 
-		static void Main(string[] args)
-		{
-			int numberOfBits = 16;
-			try
+            numberTemp = number;
+            while (numberOfOnes != numberOfOnesSmaller)
             {
-				Console.Write("Podaj dodatnią liczbę całkowitą: ");
-				uint number = uint.Parse(Console.ReadLine());
-				TheSameNumberOfOnes(Convert.ToDouble(number), numberOfBits);
-			}
-			catch(SystemException)
+                numberTemp--;
+                if (numberTemp < 0)
+                {
+                    isItSmaller = false;
+                    break;
+                }
+                binaryNumberSmaller = BinaryNumber(numberTemp, numberOfBits);
+                numberOfOnesSmaller = NumberOfOnes(numberTemp, numberOfBits);
+            }
+            Console.WriteLine("Number given: " + BinaryNumber(number, numberOfBits));
+            if (isItBigger == true)
+                Console.WriteLine("Bigger number: " + binaryNumberBigger);
+            else
+                Console.WriteLine("No number was found that satisfied the condition in the specified range.");
+            if (isItSmaller == true)
+                Console.WriteLine("Smaller number: " + binaryNumberSmaller);
+            else
+                Console.WriteLine("No number was found that satisfied the condition in the specified range.");
+        }
+
+        static void Main(string[] args)
+        {
+            int numberOfBits = 16;
+            try
             {
-				Console.WriteLine("Nie podałeś poprawnej liczby.");
-            }			
-		}
-	}
+                Console.Write("Enter a positive integer: ");
+                uint number = uint.Parse(Console.ReadLine());
+                TheSameNumberOfOnes(Convert.ToDouble(number), numberOfBits);
+            }
+            catch (SystemException)
+            {
+                Console.WriteLine("You did not enter the correct number.");
+            }
+        }
+    }
 }
