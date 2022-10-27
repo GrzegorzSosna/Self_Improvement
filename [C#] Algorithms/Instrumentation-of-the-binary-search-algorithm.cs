18ef8a2966af055327cb1340ec86aa53ddff1472
Grzegorz Sosna <grzegorz.sosna@o2.pl>
2022-10-27 12:02:51 +0200

    Update Instrumentation-of-the-binary-search-algorithm.cs

--- [C#] Algorithms/Instrumentation-of-the-binary-search-algorithm.cs
+++ [C#] Algorithms/Instrumentation-of-the-binary-search-algorithm.cs
@@ -1,7 +1,7 @@
 /* Zadanie wykonane w ramach przedmiotu "Algorytmy i struktury danych". 
 Proszę zweryfikować przedstawioną ocenę średniej i pesymistycznej złożoności wyszukiwania liniowego (10 pkt) 
 lub binarnego (20pkt). W szczególności, proszę przeprowadzić analizę za pomocą instrumentacji i pomiarów czasu. 
-W porównaniu proszę wykorzystać tablice liczb całkowitych o rozmiarze rzędu 230 bajtów (228 elementów typu 
+W porównaniu proszę wykorzystać tablice liczb całkowitych o rozmiarze rzędu 2^30 bajtów (2^28 elementów typu 
 uint/int). */
 
 using System;
@@ -33,17 +33,17 @@ namespace ConsoleApp
                 }
             }
             return false;
-        }
-        
+        }   
+
         static void BinarySearchImplementation(int[] data, int number)
         {
             if (BinarySearch(data, number))
             {
-                Console.WriteLine($"Element {number} znajduje się w zbiorze.");
+                Console.WriteLine($"Element {number} znajduje się w zbiorze liczb od 1 do {data.Length}.");
             }
             else
             {
-                Console.WriteLine($"Element {number} nie znajduje się w zbiorze.");
+                Console.WriteLine($"Element {number} nie znajduje się w zbiorze liczb od 1 do {data.Length}.");
             }
         }
 
@@ -53,12 +53,12 @@ namespace ConsoleApp
             int number = data[(data.Length - 1) / 2];
             if (BinarySearch(data, number))
             {
-                Console.WriteLine($"Złożoność minimalna: element {number} został odnaleziony w zbiorze {data.Length}-elementowym. " +
+                Console.WriteLine($"Złożoność minimalna - element {number} został odnaleziony w zbiorze {data.Length}-elementowym. " +
                     $"Liczba operacji porównania: {equalOperationCounter}");
             }
             else
             {
-                Console.WriteLine($"Złożoność minimalna: element {number} nie został odnaleziony w zbiorze {data.Length}-elementowym. " +
+                Console.WriteLine($"Złożoność minimalna - element {number} został odnaleziony w zbiorze {data.Length}-elementowym. " +
                     $"Liczba operacji porównania: {equalOperationCounter}");
             }
         }
@@ -69,12 +69,12 @@ namespace ConsoleApp
             int number = data[data.Length - 1];
             if (BinarySearch(data, number))
             {
-                Console.WriteLine($"Złożoność maksymalna: element {number} został odnaleziony w zbiorze {data.Length}-elementowym. " +
+                Console.WriteLine($"Złożoność maksymalna - element {number} został odnaleziony w zbiorze {data.Length}-elementowym. " +
                     $"Liczba operacji porównania: {equalOperationCounter}");
             }
             else
             {
-                Console.WriteLine($"Złożoność maksymalna: element {number} nie został odnaleziony w zbiorze {data.Length}-elementowym. " +
+                Console.WriteLine($"Złożoność maksymalna - element {number} został odnaleziony w zbiorze {data.Length}-elementowym. " +
                     $"Liczba operacji porównania: {equalOperationCounter}");
             }
         }
@@ -88,17 +88,17 @@ namespace ConsoleApp
                 int number = data[i];
                 if (BinarySearch(data, number))
                 {
-                    Console.WriteLine($"Złożoność średnia - iteracja: element {number} został odnaleziony w zbiorze {data.Length}-elementowym. " +
+                    Console.WriteLine($"Złożoność średnia - element {number} został odnaleziony w zbiorze {data.Length}-elementowym. " +
                         $"Liczba operacji porównania: {equalOperationCounter}");
                 }
                 else
                 {
-                    Console.WriteLine($"Złożoność średnia - iteracja: element {number} nie został odnaleziony w zbiorze {data.Length}-elementowym. " +
+                    Console.WriteLine($"Złożoność średnia - element {number} został odnaleziony w zbiorze {data.Length}-elementowym. " +
                         $"Liczba operacji porównania: {equalOperationCounter}");
                 }
                 totalOperationsCounter += equalOperationCounter;
             }
-            Console.WriteLine($"Złożoność średnia - iteracja: łączna liczba operacji porównania {totalOperationsCounter}, " +
+            Console.WriteLine($"[Podsumowanie] Złożoność średnia - łączna liczba operacji porównania: {totalOperationsCounter}, " +
                     $"liczba iteracji: {data.Length}, średnia operacji porównania: {totalOperationsCounter / (ulong)data.Length}");
         }
 
@@ -117,12 +117,12 @@ namespace ConsoleApp
 
                 if (BinarySearch(data, number))
                 {
-                    Console.WriteLine($"Złożoność minimalna: element {number} został odnaleziony w zbiorze {data.Length}-elementowym. " +
+                    Console.WriteLine($"Złożoność minimalna - element {number} został odnaleziony w zbiorze {data.Length}-elementowym. " +
                         $"Czas operacji: {(iterationElapsedTime * (1.0 / Stopwatch.Frequency)).ToString("F10")}");
                 }
                 else
                 {
-                    Console.WriteLine($"Złożoność minimalna: element {number} nie został odnaleziony w zbiorze {data.Length}-elementowym. " +
+                    Console.WriteLine($"Złożoność minimalna - element {number} nie został odnaleziony w zbiorze {data.Length}-elementowym. " +
                         $"Czas operacji: {(iterationElapsedTime * (1.0 / Stopwatch.Frequency)).ToString("F10")}");
                 }
 
@@ -139,7 +139,7 @@ namespace ConsoleApp
 
             elapsedTime -= (minTime + maxTime);
             double elapsedSeconds = elapsedTime * (1.0 / (iterationsNumber * Stopwatch.Frequency));
-            Console.WriteLine($"Złożoność minimalna: liczba powtórzeń: {iterationsNumber}, średni czas przebiegu operacji: {elapsedSeconds.ToString("F10")} [s], zakładając odrzucenie czasów skrajnych.");
+            Console.WriteLine($"[Podsumowanie] Złożoność minimalna - liczba powtórzeń: {iterationsNumber}, średni czas przebiegu operacji: {elapsedSeconds.ToString("F10")} [s], zakładając odrzucenie czasów skrajnych.");
         }
 
         static void BinarySearchTimeMax(int[] data)
@@ -157,15 +157,15 @@ namespace ConsoleApp
 
                 if (BinarySearch(data, number))
                 {
-                    Console.WriteLine($"Złożoność maksymalna: element {number} został odnaleziony w zbiorze {data.Length}-elementowym. " +
-                        $"Czas operacji: {(iterationElapsedTime * (1.0 / Stopwatch.Frequency)).ToString("F10")}");
+                    Console.WriteLine($"Złożoność maksymalna - element {number} został odnaleziony w zbiorze {data.Length}-elementowym. " +
+                        $"Czas operacji: {(iterationElapsedTime * (1.0 / Stopwatch.Frequency)).ToString("F10")} [s]");
                 }
                 else
                 {
-                    Console.WriteLine($"Złożoność maksymalna: element {number} nie został odnaleziony w zbiorze {data.Length}-elementowym. " +
-                        $"Czas operacji: {(iterationElapsedTime * (1.0 / Stopwatch.Frequency)).ToString("F10")}");
+                    Console.WriteLine($"Złożoność maksymalna - element {number} nie został odnaleziony w zbiorze {data.Length}-elementowym. " +
+                        $"Czas operacji: {(iterationElapsedTime * (1.0 / Stopwatch.Frequency)).ToString("F10")} [s]");
                 }
-                
+
                 elapsedTime += iterationElapsedTime;
                 if (iterationElapsedTime < minTime)
                 {
@@ -178,7 +178,8 @@ namespace ConsoleApp
             }
             elapsedTime -= (minTime + maxTime);
             double elapsedSeconds = elapsedTime * (1.0 / (iterationsNumber * Stopwatch.Frequency));
-            Console.WriteLine($"Złożoność maksymalna: liczba powtórzeń: {iterationsNumber}, średni czas przebiegu operacji: {elapsedSeconds.ToString("F10")} [s], zakładając odrzucenie czasów skrajnych.");
+            Console.WriteLine($"[Podsumowanie] Złożoność maksymalna - liczba powtórzeń: {iterationsNumber}, " +
+                $"średni czas przebiegu operacji: {elapsedSeconds.ToString("F10")} [s] - zakładając odrzucenie czasów skrajnych.");
         }
 
         static void BinarySearchTimeAvg(int[] data)
@@ -199,13 +200,13 @@ namespace ConsoleApp
 
                     if (BinarySearch(data, number))
                     {
-                        Console.WriteLine($"Złożoność średnia (iteracja {iterationsNumber}): element {number} został odnaleziony w zbiorze: {data.Length}-elementowym, " +
-                            $"średni czas operacji: {(iterationElapsedTime * (1.0 / Stopwatch.Frequency)).ToString("F10")} [s]."); 
+                        Console.WriteLine($"Złożoność średnia ({iterationsNumber} iteracji) - element {number} został odnaleziony w zbiorze: {data.Length}-elementowym, " +
+                            $"średni czas operacji: {(iterationElapsedTime * (1.0 / Stopwatch.Frequency)).ToString("F10")} [s]"); 
                     }
                     else
                     {
-                        Console.WriteLine($"Złożoność średnia (iteracja {iterationsNumber}): element {number} został odnaleziony w zbiorze: {data.Length}-elementowym, " +
-                            $"średni czas operacji: {(iterationElapsedTime * (1.0 / Stopwatch.Frequency)).ToString("F10")} [s].");
+                        Console.WriteLine($"Złożoność średnia ({iterationsNumber} iteracji) - element {number} został odnaleziony w zbiorze: {data.Length}-elementowym, " +
+                            $"średni czas operacji: {(iterationElapsedTime * (1.0 / Stopwatch.Frequency)).ToString("F10")} [s]");
                     }
                     elapsedTime += iterationElapsedTime;
                     if (iterationElapsedTime < minTime)
@@ -220,27 +221,20 @@ namespace ConsoleApp
             }
             elapsedTime -= (minTime + maxTime);
             double elapsedSeconds = elapsedTime * (1.0 / (iterationsNumber * Stopwatch.Frequency));
-            Console.WriteLine($"Złożoność średnia: liczba powtórzeń: {iterationsNumber}, " +
-                $"średni czas przebiegu operacji: {(elapsedSeconds / data.Length).ToString("F10")} [s], zakładając odrzucenie czasów skrajnych.");
+            Console.WriteLine($"[Podsumowanie] Złożoność średnia - liczba powtórzeń: {iterationsNumber}, " +
+                $"średni czas przebiegu operacji: {(elapsedSeconds / data.Length).ToString("F10")} [s] - zakładając odrzucenie czasów skrajnych.");
         }
 
         static void Main(string[] args)
         {
-            int[] data = new int[100] {
-                1, 2, 3, 4, 5, 6, 7, 8, 9, 10
-                , 11, 12, 13, 14, 15, 16, 17, 18, 19, 20
-                , 21, 22, 23, 24, 25, 26, 27, 28, 29, 30
-                , 31, 32, 33, 34, 35, 36, 37, 38, 39, 40
-                , 41, 42, 43, 44, 45, 46, 47, 48, 49, 50
-                , 51, 52, 53, 54, 55, 56, 57, 58, 59, 60
-                , 61, 62, 63, 64, 65, 66, 67, 68, 69, 70
-                , 71, 72, 73, 74, 75, 76, 77, 78, 79, 80
-                , 81, 82, 83, 84, 85, 86, 87, 88, 89, 90
-                , 91, 92, 93, 94 ,95, 96, 97, 98, 99, 100
-            };
-
-            Console.WriteLine("Implementacja algorytmu bez oceny złożoności dla przykładowej liczby 10.");
-            int number = 10;
+            int[] data = new int[100];
+            for (int i = 0; i < data.Length; i++)
+            {
+                data[i] = i + 1;
+            }
+      
+            int number = 1000;
+            Console.WriteLine($"Implementacja algorytmu bez oceny złożoności dla przykładowej liczby {number}.");
             BinarySearchImplementation(data, number);
 
             //BinarySearchMin(data);
