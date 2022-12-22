f669a12d2f4a5d1f586b59169a9073d4f8c5a962
Grzegorz Sosna <grzegorz.sosna@o2.pl>
2022-12-22 12:31:55 +0100

    Update Instrumentation-and-comparison-of-different-sorting-algorithms.cs

--- [C#] Algorithms/Instrumentation-and-comparison-of-different-sorting-algorithms.cs
+++ [C#] Algorithms/Instrumentation-and-comparison-of-different-sorting-algorithms.cs
@@ -10,7 +10,6 @@ namespace ConsoleApp
         static int[] InsertionSort(int[] array)
         {
             equalOperationCounter = 0;
-
             for (int i = 0; i < array.Length; i++)
             {
                 for (int j = i; j > 0; j--)
@@ -28,49 +27,150 @@ namespace ConsoleApp
             return array;
         }
 
-        static void Main(string[] args)
+        static void RandomData(int size, int selectedAlgorithm)
         {
-            Console.WriteLine("(1) Instrumentacja algorytmu sortujacego przez wstawianie (Insertion Sort):");
-
-            // dane w postaci losowej
+            int[] array = new int[size];
+            int[] sortedArray = new int[size];
             Console.WriteLine("\n-> Tablica liczb generowanych w postaci LOSOWEJ");
-            int[] array = new int[20];
             Random numberRnd = new Random();
-            for (int i = 0; i < array.Length; i++)
-            {
-                array[i] = numberRnd.Next(1,21);
-            }
             Console.WriteLine("\nTablica nieposortowana:");
             for (int i = 0; i < array.Length; i++)
             {
-                Console.Write(array[i] + ", ");
+                array[i] = numberRnd.Next(1, array.Length + 1);
+                Console.Write(array[i]);
+                if (i < array.Length - 1)
+                    Console.Write(", ");
             }
-            int[] sortedArray = InsertionSort(array);
+            if (selectedAlgorithm == 1)
+                sortedArray = InsertionSort(array);
+
             Console.WriteLine("\nTablica posortowana:");
             for (int i = 0; i < sortedArray.Length; i++)
             {
-                Console.Write(sortedArray[i] + ", ");
+                Console.Write(sortedArray[i]);
+                if (i < array.Length - 1)
+                    Console.Write(", ");
             }
             Console.WriteLine("\nLiczba operacji potrzebnych do posortowania tablicy: " + equalOperationCounter);
+        }
 
-            // dane w postaci rosnącej
+        static void GrowingData(int size, int selectedAlgorithm)
+        {
+            int[] array = new int[size];
+            int[] sortedArray = new int[size];
             Console.WriteLine("\n-> Tablica liczb generowanych w postaci ROSNĄCEJ");
+            Console.WriteLine("\nTablica nieposortowana:");
             for (int i = 0; i < array.Length; i++)
             {
                 array[i] = i + 1;
+                Console.Write(array[i]);
+                if (i < array.Length - 1)
+                    Console.Write(", ");
             }
-            Console.WriteLine("\nTablica nieposortowana:");
-            for (int i = 0; i < array.Length; i++)
-            {
-                Console.Write(array[i] + ", ");
-            }
-            sortedArray = InsertionSort(array);
+            if (selectedAlgorithm == 1)
+                sortedArray = InsertionSort(array);
+
             Console.WriteLine("\nTablica posortowana:");
             for (int i = 0; i < sortedArray.Length; i++)
             {
-                Console.Write(sortedArray[i] + ", ");
+                Console.Write(sortedArray[i]);
+                if (i < array.Length - 1)
+                    Console.Write(", ");
             }
             Console.WriteLine("\nLiczba operacji potrzebnych do posortowania tablicy: " + equalOperationCounter);
+        }
+
+        static void DecreasingData(int size, int selectedAlgorithm)
+        {
+            int[] array = new int[size];
+            int[] sortedArray = new int[size];
+            Console.WriteLine("\n-> Tablica liczb generowanych w postaci MALEJĄCEJ");
+            Console.WriteLine("\nTablica nieposortowana:");
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = array.Length - i;
+                Console.Write(array[i]);
+                if (i < array.Length - 1)
+                    Console.Write(", ");
+            }
+            if (selectedAlgorithm == 1)
+                sortedArray = InsertionSort(array);
+
+            Console.WriteLine("\nTablica posortowana:");
+            for (int i = 0; i < sortedArray.Length; i++)
+            {
+                Console.Write(sortedArray[i]);
+                if (i < array.Length - 1)
+                    Console.Write(", ");
+            }
+            Console.WriteLine("\nLiczba operacji potrzebnych do posortowania tablicy: " + equalOperationCounter);
+        }
+
+        static void FixedData(int size, int selectedAlgorithm)
+        {
+            int[] array = new int[size];
+            int[] sortedArray = new int[size];
+            Console.WriteLine("\n-> Tablica liczb generowanych w postaci STAŁEJ");
+            Console.WriteLine("\nTablica nieposortowana:");
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = array.Length / 2;
+                Console.Write(array[i]);
+                if (i < array.Length - 1)
+                    Console.Write(", ");
+            }
+            if (selectedAlgorithm == 1)
+                sortedArray = InsertionSort(array);
+
+            Console.WriteLine("\nTablica posortowana:");
+            for (int i = 0; i < sortedArray.Length; i++)
+            {
+                Console.Write(sortedArray[i]);
+                if (i < array.Length - 1)
+                    Console.Write(", ");
+            }
+            Console.WriteLine("\nLiczba operacji potrzebnych do posortowania tablicy: " + equalOperationCounter);
+        }
+
+        static void VData(int size, int selectedAlgorithm)
+        {
+            int[] array = new int[size];
+            int[] sortedArray = new int[size];
+            Console.WriteLine("\n-> Tablica liczb generowanych w postaci V-KSZTAŁTNEJ");
+            Console.WriteLine("\nTablica nieposortowana:");
+            int numberRnd = array.Length / 2 + 1;
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = numberRnd;
+                if (i < array.Length / 2)
+                    numberRnd--;
+                else
+                    numberRnd++;               
+                Console.Write(array[i]);
+                if (i < array.Length - 1)
+                    Console.Write(", ");
+            }
+            if (selectedAlgorithm == 1)
+                sortedArray = InsertionSort(array);
+
+            Console.WriteLine("\nTablica posortowana:");
+            for (int i = 0; i < sortedArray.Length; i++)
+            {
+                Console.Write(sortedArray[i]);
+                if (i < array.Length - 1)
+                    Console.Write(", ");
+            }
+            Console.WriteLine("\nLiczba operacji potrzebnych do posortowania tablicy: " + equalOperationCounter);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("(1) Instrumentacja algorytmu sortujacego przez wstawianie (Insertion Sort):");
+            RandomData(21,1);
+            GrowingData(21, 1);
+            DecreasingData(21, 1);
+            FixedData(21, 1);
+            VData(21, 1);
 
         }
     }
