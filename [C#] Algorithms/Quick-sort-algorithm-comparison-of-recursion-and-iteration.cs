553bda64958514f2a8a5970086a2a6234febdc96
Grzegorz Sosna <grzegorz.sosna@o2.pl>
2023-01-31 12:11:13 +0100

    Update Quick-sort-algorithm-comparison-of-recursion-and-iteration.cs

--- [C#] Algorithms/Quick-sort-algorithm-comparison-of-recursion-and-iteration.cs
+++ [C#] Algorithms/Quick-sort-algorithm-comparison-of-recursion-and-iteration.cs
@@ -13,15 +13,16 @@ namespace ConsoleApp
         private static ulong equalOperationCounter;
 
         // iteracyjna implementacja sortowania Quick Sort 
-        static int[] QuickSortIteration(int[] array)
+        static int[] QuickSortIteration(int[] array, string pivotType)
         {
             equalOperationCounter = 0;
             int leftElement, rightElement;
             int[] stackLeft = new int[array.Length];
             int[] stackRight = new int[array.Length];
+            int pivot = 0;
             int border = 0;
             stackLeft[border] = 0;
-            stackRight[border] = array.Length - 1;
+            stackRight[border] = array.Length - 1;            
 
             while (border >= 0)
             {
@@ -33,7 +34,14 @@ namespace ConsoleApp
                 {
                     int i = leftElement;
                     int j = rightElement;
-                    int pivot = array[(leftElement + rightElement) / 2];
+					
+                    if (pivotType == "middlePivot")
+                        pivot = array[(leftElement + rightElement) / 2];
+                    if (pivotType == "rightmostPivot")
+                        pivot = array[rightElement];
+                    Random rnd = new Random();
+                    if (pivotType == "randomPivot")
+                        pivot = array[rnd.Next(rightElement)];
 
                     while (i <= j)
                     {
@@ -66,12 +74,20 @@ namespace ConsoleApp
         }
 
         // rekurencyjna implementacja sortowania Quick Sort 
-        static int[] QuickSortRecursion(int[] array, int l, int p)
+        static int[] QuickSortRecursion(int[] array, int l, int p, string pivotType)
         {
             int i, j;
+            int pivot = 0;
             i = l;
             j = p;
-            int pivot = array[(l + p) / 2]; 
+
+            if (pivotType == "middlePivot")
+                pivot = array[(l + p) / 2];
+            if (pivotType == "rightmostPivot")
+                pivot = array[p];
+            Random rnd = new Random();
+            if (pivotType == "randomPivot")
+                pivot = array[rnd.Next(p)];
 
             do
             {
@@ -92,34 +108,40 @@ namespace ConsoleApp
             while (i <= j);
 
             if (l < j)
-                QuickSortRecursion(array, l, j);
+                QuickSortRecursion(array, l, j, pivotType);
             if (i < p)
-                QuickSortRecursion(array, i, p);
+                QuickSortRecursion(array, i, p, pivotType);
 
             return array;
-        }
+        }        
 
         // wybór algorytmu sortowania oraz pivota
         static int[] SelectedAlgorithm(int choice, int[] array)
         {
-            // wybór pivota
-            int leftElement = 0;
-            int rightElement = array.Length - 1;
-            int middlePivot = array[(leftElement + rightElement) / 2];
-            int rightmostPivot = array[rightElement];
-            Random rnd = new Random();
-            int randomPivot = rnd.Next(array.Length);
-
             if (choice == 1)
-                return QuickSortIteration(array);
+                return QuickSortIteration(array, "middlePivot");
             if (choice == 2)
             {
                 equalOperationCounter = 0;
-                return QuickSortRecursion(array, 0, array.Length - 1);
-            }             
+                return QuickSortRecursion(array, 0, array.Length - 1, "middlePivot");
+            }
+            if (choice == 3)
+                return QuickSortIteration(array, "rightmostPivot");
+            if (choice == 4)
+            {
+                equalOperationCounter = 0;
+                return QuickSortRecursion(array, 0, array.Length - 1, "rightmostPivot");
+            }
+            if (choice == 5)
+                return QuickSortIteration(array, "randomPivot");
+            if (choice == 6)
+            {
+                equalOperationCounter = 0;
+                return QuickSortRecursion(array, 0, array.Length - 1, "randomPivot");
+            }
             return array;
         }
-
+        
         // generowanie tablicy w postaci losowej
         static void RandomData(int size, int selectedAlgorithm)
         {
@@ -180,13 +202,30 @@ namespace ConsoleApp
 
         static void Main(string[] args)
         {
-            Console.WriteLine("(1) Instrumentacja algorytmu Quick Sort - implementacja iteracyjna:");
+            Console.WriteLine("(1) Instrumentacja algorytmu Quick Sort (implementacja: iteracyjna, pivot: środkowy)");
             RandomData(21, 1);
             VData(21, 1);
 
-            Console.WriteLine("\n\n(2) Instrumentacja algorytmu Quick Sort - implementacja rekurencyjna:");
+            Console.WriteLine("\n\n(2) Instrumentacja algorytmu Quick Sort (implementacja: rekurencyjna, pivot: środkowy)");
             RandomData(21, 2);
             VData(21, 2);
+
+            Console.WriteLine("\n\n(3) Instrumentacja algorytmu Quick Sort (implementacja: iteracyjna, pivot: skrajnie prawy)");
+            RandomData(21, 3);
+            VData(21, 3);
+
+            Console.WriteLine("\n\n(4) Instrumentacja algorytmu Quick Sort (implementacja: rekurencyjna, pivot: skrajnie prawy)");
+            RandomData(21, 4);
+            VData(21, 4);
+
+            Console.WriteLine("\n\n(5) Instrumentacja algorytmu Quick Sort (implementacja: iteracyjna, pivot: losowy)");
+            RandomData(21, 5);
+            VData(21, 5);
+
+            Console.WriteLine("\n\n(6) Instrumentacja algorytmu Quick Sort (implementacja: rekurencyjna, pivot: losowy)");
+            RandomData(21, 6);
+            VData(21, 6);
+
         }
     }
 }
